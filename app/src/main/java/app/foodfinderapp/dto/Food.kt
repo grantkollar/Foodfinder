bf0dccf1e969ace4998161e81c930de7a8daeaba
Tony Gentile <91705356+TGentile6@users.noreply.github.com>
2023-03-05 18:10:24 -0500

    Updated DTO classes to use "data" tag

--- app/src/main/java/app/foodfinderapp/dto/Food.kt
+++ app/src/main/java/app/foodfinderapp/dto/Food.kt
@@ -1,6 +1,6 @@
 package app.foodfinderapp.dto
 
-class Food(var foodID:Int, var foodName: String, var foodPrice: Float, var foodCategory: String) {
+data class Food(var foodID:Int, var foodName: String, var foodPrice: Float, var foodCategory: String) {
     override fun toString(): String {
         return "$foodID $foodName $foodPrice $foodCategory"
     }
--- app/src/main/java/app/foodfinderapp/dto/Restaurant.kt
+++ app/src/main/java/app/foodfinderapp/dto/Restaurant.kt
@@ -2,7 +2,7 @@ package app.foodfinderapp.dto
 
 import java.util.Date
 
-class Restaurant(val restaurantID : Int, var name:String,var category : String, var hours: String, var contact: String){
+data class Restaurant(val restaurantID : Int, var name:String,var category : String, var hours: String, var contact: String){
     override fun toString(): String {
         return "$restaurantID $name $category $hours $contact"
     }
