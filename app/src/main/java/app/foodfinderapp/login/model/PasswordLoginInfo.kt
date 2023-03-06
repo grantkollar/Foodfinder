bec60e398beb41e86f96ae35f941d3646058f473
Tony Gentile <91705356+TGentile6@users.noreply.github.com>
2023-03-05 19:45:24 -0500

    Moved redundant "DetailUser" class declarations to independent class file

--- app/src/main/java/app/foodfinderapp/login/model/PasswordLoginInfo.kt
+++ app/src/main/java/app/foodfinderapp/login/model/PasswordLoginInfo.kt
@@ -1,5 +1,5 @@
 package app.foodfinderapp.login.model
 
-class PasswordLoginInfo(var code: String,val user:DetailUser){
-    class DetailUser (val user: User, val token:String, val status:Int)
+class PasswordLoginInfo(var code: String,val user : DetailUser){
+
 }
--- app/src/main/java/app/foodfinderapp/login/model/UserRespose.kt
+++ app/src/main/java/app/foodfinderapp/login/model/UserRespose.kt
@@ -1,5 +1,4 @@
 package app.foodfinderapp.login.model
 
 class UserResponse(var code:String, val detailUser: DetailUser){
-    class DetailUser (val user: User, val token:String, val status:Int)
 }
