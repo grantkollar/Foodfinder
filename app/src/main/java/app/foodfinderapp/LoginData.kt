a51412a5f21119ffa07c091e140126c8ad9f3f9b
Tony Gentile <91705356+TGentile6@users.noreply.github.com>
2023-03-05 19:39:15 -0500

    Undid change to constant values

--- app/src/main/java/app/foodfinderapp/LoginData.kt
+++ app/src/main/java/app/foodfinderapp/LoginData.kt
@@ -2,12 +2,12 @@ package app.foodfinderapp
 
 class LoginData {
     companion object{
-        const val LOGIN_STATUS: Int = 0
+        var LOGIN_STATUS: Int = 0
         const val CODE_COOKIE: String = ""
-        const val SAVE_COOKIE: Int = 0
-        const val TURN_MAIN: Int = 0
+        var SAVE_COOKIE: Int = 0
+        var TURN_MAIN: Int = 0
         var size:Int = 0
-        const val GET_ADDRESS = 0
-        const val DELETE:Int = 0
+        var GET_ADDRESS = 0
+        var DELETE:Int = 0
     }
 }
