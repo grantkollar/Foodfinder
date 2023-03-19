04b1a8975d0e1606105b447bf14acd789621a2e1
Yifan Bian <bianya@mail.uc.edu>
2023-03-19 13:28:00 -0400

    update restaurant dto

--- app/src/main/java/app/foodfinderapp/dto/Restaurant.kt
+++ app/src/main/java/app/foodfinderapp/dto/Restaurant.kt
@@ -2,7 +2,9 @@ package app.foodfinderapp.dto
 
 import java.util.Date
 
-data class Restaurant(val restaurantID : Int, var name:String,var category : String, var hours: String, var contact: String){
+data class Restaurant(
+    val restaurantID: String, var name:String,
+    var category: String, var hours: String, var contact: String, var address: String){
     override fun toString(): String {
         return "$restaurantID $name $category $hours $contact"
     }
