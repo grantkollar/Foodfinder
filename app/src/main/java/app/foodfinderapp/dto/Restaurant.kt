d33ed8570b4b0ec9f0b328919711f6b8135a09f0
Yifan Bian <bianya@mail.uc.edu>
2023-03-19 13:50:52 -0400

    add default constructor

--- app/src/main/java/app/foodfinderapp/dto/Restaurant.kt
+++ app/src/main/java/app/foodfinderapp/dto/Restaurant.kt
@@ -5,6 +5,7 @@ import java.util.Date
 data class Restaurant(
     val restaurantID: String, var name:String,
     var category: String, var hours: String, var contact: String, var address: String){
+    constructor() : this("", "", "", "", "", "")
     override fun toString(): String {
         return "$restaurantID $name $category $hours $contact"
     }
