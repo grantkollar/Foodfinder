9411d2f8b4d981939f70554d1a3624d86a322f25
Yifan Bian <bianya@mail.uc.edu>
2023-03-19 13:27:28 -0400

    add recycle view adapter

--- app/src/main/java/app/foodfinderapp/adapter/RestaurantAdapter.kt
+++ app/src/main/java/app/foodfinderapp/adapter/RestaurantAdapter.kt
@@ -1,4 +1,42 @@
 package app.foodfinderapp.adapter
 
-class RestaurantAdapter {
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import app.foodfinderapp.R
+import app.foodfinderapp.databinding.RestaurantItemBinding
+import app.foodfinderapp.dto.Restaurant
+
+class RestaurantAdapter(private var restaurantList: List<Restaurant>) :
+    RecyclerView.Adapter<RestaurantAdapter.RestaurantViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RestaurantViewHolder {
+
+        val binding = RestaurantItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return RestaurantViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RestaurantViewHolder, position: Int) {
+        val currentRestaurant = restaurantList[position]
+        holder.bind(currentRestaurant)
+    }
+
+    override fun getItemCount() = restaurantList.size
+
+    inner class RestaurantViewHolder(private val binding: RestaurantItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(restaurant: Restaurant) {
+            binding.restaurantName.text = restaurant.name
+            binding.restaurantAddress.text = restaurant.address
+            // 可以继续设置其他控件的属性
+        }
+    }
+    fun setDataList(newList: List<Restaurant>) {
+        restaurantList = newList.toMutableList()
+    }
+
 }
