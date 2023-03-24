593597ccd59711283790aaee263ec10163ef519e
grantkollar <89742140+grantkollar@users.noreply.github.com>
2023-03-24 18:00:28 -0400

    dashboard fetching

--- app/src/main/java/app/foodfinderapp/DashboardFragment.kt
+++ app/src/main/java/app/foodfinderapp/DashboardFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import app.foodfinderapp.databinding.ActivityLoginBindingImpl
 import app.foodfinderapp.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
---
+++ app/src/main/java/app/foodfinderapp/HomeActiv.kt
@@ -0,0 +1,49 @@
+package app.foodfinderapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import app.foodfinderapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
+
+class HomeActiv : AppCompatActivity() {
+    private val db = Firebase.firestore
+
+    private lateinit var name : TextView
+    private lateinit var firebaseAuth : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.fragment_home)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val uid = firebaseAuth.currentUser?.uid
+
+        name = findViewById(R.id.dashBoardHeader)
+
+        val docRef = db.collection("restaurant").document(uid!!)
+        docRef.get()
+            .addOnSuccessListener {document ->
+                if(document != null){
+                    val fName = document.data!!["name"].toString()
+
+
+                    name.text = fName
+                }
+
+            }
+            .addOnFailureListener{exception ->
+                Toast.makeText(this,"failed",Toast.LENGTH_SHORT).show()
+
+            }
+
+    }
+
+}
