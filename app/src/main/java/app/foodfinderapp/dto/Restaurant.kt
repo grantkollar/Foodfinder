0bee619d98737d94ee746cf7e67133c9a538f13d
Subu <SUBRAMANIYAN_ANNAMALAI@hotmail.com>
2023-03-30 18:07:42 -0400

    added trailing commas due to best practice to make easy manipulation of elements. Also i changed id related variables to val instead of var as they won't change as they are like unique identifiers

--- app/src/main/java/app/foodfinderapp/dto/Restaurant.kt
+++ app/src/main/java/app/foodfinderapp/dto/Restaurant.kt
@@ -3,8 +3,13 @@ package app.foodfinderapp.dto
 import java.util.Date
 
 data class Restaurant(
-    val restaurantID: String, var name:String,
-    var category: String, var hours: String, var contact: String, var address: String){
+    val restaurantID: String,
+    var name:String,
+    var category: String,
+    var hours: String,
+    var contact: String,
+    var address: String
+    ){
     constructor() : this("", "", "", "", "", "")
     override fun toString(): String {
         return "$restaurantID $name $category $hours $contact"
